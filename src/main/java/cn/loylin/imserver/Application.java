d7e0d025bd3693261eb8eeb780954b9ba98d4e9c
Loylin <Qq935020188>
2018-03-12 23:06:03 +0800

    ImServer Created
    Signed-off-by: Loylin <Qq935020188>

---
+++ src/main/java/cn/loylin/imserver/Application.java
@@ -0,0 +1,8 @@
+package cn.loylin.imserver;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
---
+++ src/test/java/cn/loylin/imserver/ApplicationTest.java
@@ -0,0 +1,8 @@
+package cn.loylin.imserver;
+
+public class ApplicationTest {
+
+    public static void main(String[] args) {
+        System.out.println("Test!");
+    }
+}
